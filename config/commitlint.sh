a93ab1a5d4f55e5141cd0bee146a420d11ee8f8f
Bella Nicholson <nicholsonbella@gmail.com>
2024-04-02 22:10:00 +0200

    feat: added more pre-commit hook actions

--- config/commitlint.sh
+++ config/commitlint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Extract commit messsage
+# 1. Extract commit messsage
 commit_message=$(git log -1 --pretty=%B)
 
-# Apply commitlint tool in CLI
+# 2. Check against allowed exceptions
+exceptions=( 'Initial commit' )
+for exception in "${exceptions[@]}"; do
+  if [[ "$commit_message" == "$exception" ]]; then
+    exit 0
+  fi
+done
+
+# 3. If no match found, apply commitlint tool in CLI
 echo "$commit_message" | commitlint --config config/commitlint.config.js
