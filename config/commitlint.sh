9959cc48df3dc122aac6fa2519a3cb267eecbdae
Bella Nicholson <nicholsonbella@gmail.com>
2024-04-19 22:05:53 +0200

    feat: simplified commitlint approach

--- config/commitlint.sh
+++ config/commitlint.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 # 1. Extract commit messsage
-commit_message=$(git log -1 --pretty=%B)
+COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+echo "$COMMIT_MESSAGE"
 
 # 2. Check against allowed exceptions
 exceptions=( 'Initial commit' )
 for exception in "${exceptions[@]}"; do
-  if [[ "$commit_message" == "$exception" ]]; then
+  if [[ "$COMMIT_MESSAGE" == "$exception" ]]; then
     exit 0
   fi
 done
 
 # 3. If no match found, apply commitlint tool in CLI
-echo "$commit_message" | commitlint --config config/commitlint.config.js
+# Refer to https://www.conventionalcommits.org/en/v1.0.0/ for additional guidance
+REGEX_PATTERN='^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test){1}(\([[:alnum:]._-]+\))?(!)?: ([[:alnum:]])+([[:space:][:print:]]*)|^Merge|^pick [a-f0-9]+ (.+)$|^Revert\s'
+if [[ ! $COMMIT_MESSAGE =~ $REGEX_PATTERN ]]; then
+  echo "ERROR: Commit message does not follow the Conventional Commits format"
+  echo "For Conventional Commits format specifications, please refer to https://www.conventionalcommits.org/en/v1.0.0/"
+  exit 1
+else
+  echo "Everything is okay!"
+fi
