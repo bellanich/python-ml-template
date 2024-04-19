a64bc3016b9a80c3918601a6acdf91613aaa6142
Bella Nicholson <nicholsonbella@gmail.com>
2024-04-19 22:08:53 +0200

    docs: added link to source for regex pattern in code

--- config/commitlint.sh
+++ config/commitlint.sh
@@ -13,7 +13,7 @@ for exception in "${exceptions[@]}"; do
 done
 
 # 3. If no match found, apply commitlint tool in CLI
-# Refer to https://www.conventionalcommits.org/en/v1.0.0/ for additional guidance
+# Regex pattern taken from https://gist.github.com/marcojahn/482410b728c31b221b70ea6d2c433f0c
 REGEX_PATTERN='^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test){1}(\([[:alnum:]._-]+\))?(!)?: ([[:alnum:]])+([[:space:][:print:]]*)|^Merge|^pick [a-f0-9]+ (.+)$|^Revert\s'
 if [[ ! $COMMIT_MESSAGE =~ $REGEX_PATTERN ]]; then
   echo "ERROR: Commit message does not follow the Conventional Commits format"
